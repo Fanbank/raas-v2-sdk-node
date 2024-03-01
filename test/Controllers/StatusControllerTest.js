b604819a49b34402fcfa42c2587ec66379f3041d
Scott Geye <scottgeye@gmail.com>
2024-02-29 16:27:17 -0800

    audit fix

--- test/Controllers/StatusControllerTest.js
+++ test/Controllers/StatusControllerTest.js
@@ -4,34 +4,38 @@
  * This file was automatically generated for Tango Card, Inc. by APIMATIC v2.0 ( https://apimatic.io ).
  */
 
-'use strict';
-
 const chai = require('chai');
-const assert = chai.assert;
-const TestHelper = require("../TestHelper");
-const APIHelper = require("../../lib/APIHelper");
-const testerlib = require("../../lib");
-const testConfiguration = require("../TestBootstrap");
+
+const { assert } = chai;
+const TestHelper = require('../TestHelper');
+const testerlib = require('../../lib');
+const testConfiguration = require('../TestBootstrap');
 
 const controller = testerlib.StatusController;
-const SystemStatusResponseModel = testerlib.SystemStatusResponseModel;
 
-describe("StatusController Tests", function tests() {
-    this.timeout(testConfiguration.TEST_TIMEOUT);
+describe('StatusController Tests', function tests() {
+  this.timeout(testConfiguration.TEST_TIMEOUT);
 
-    /**
-     * Tests if we can retrieve the system status
-     */
-    it("should testStatusIsOK response", function testStatusIsOKTest(done) {
-        controller.getSystemStatus(function callback(error, response, context) {
-            // test response code
-            assert.equal(200, context.response.statusCode);
-            assert.isNotNull(response);
-            assert.isTrue(TestHelper.isProperSubsetOf(context.response.body, {
-  "status" : "UP"
-}, false, true, false));
-            done();
-        });
+  /**
+   * Tests if we can retrieve the system status
+   */
+  it('should testStatusIsOK response', function testStatusIsOKTest(done) {
+    controller.getSystemStatus(function callback(error, response, context) {
+      // test response code
+      assert.equal(200, context.response.statusCode);
+      assert.isNotNull(response);
+      assert.isTrue(
+        TestHelper.isProperSubsetOf(
+          context.response.body,
+          {
+            status: 'UP',
+          },
+          false,
+          true,
+          false
+        )
+      );
+      done();
     });
-
+  });
 });
